de0621fbd94e942c07415c3985e6ef78f8ddc9df
Sami Heikkinen <sheikki@yahoo.com>
2026-05-21 12:17:44 +0300

    chore(release): v0.5.3 changelog freeze

--- yamlgraph/__init__.py
+++ yamlgraph/__init__.py
@@ -4,7 +4,7 @@ Framework for building LLM pipelines with YAML configuration.
 State is generated dynamically from graph config.
 """
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 
 from pathlib import Path
 
